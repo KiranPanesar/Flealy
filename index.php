27ee76fca1f8f9bcd548c3f8da1081134245ae44
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-18 23:59:58 +0000

    Moved home code into index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE>
+
+<html>
+
+<head>
+	<title>Flea.ly</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/home.css"/>
+	<script src="js/script.js"></script>
+</head>
+
+<body>
+
+	<div id="welcome-message">
+		<h1>Welcome to <a href="#">Flea.ly</a></h1>
+		<h2>Your local market</h2>
+	</div>
+	<br/>
+
+	<div id="get-started">
+		<form id="postcode-form">
+			<input type="text" name="postcode" id="postcode-input" class="text-input" placeholder="Enter your postcode" onkeyup="validatePostcode()" /><br/><br/>
+			<input type="submit" value="Start Shopping" class="btn-submit" id="start-shopping" />
+		</form>
+		<input type="submit" value="Sign Up" class="btn-submit" id="sign-up" onclick="signUp();" />
+	</div>
+</body>
+
+<script src="js/home.js"></script>
+
+</html>
