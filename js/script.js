1e16ebd226cc47d51560255816e837da30c6bb65
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-19 15:35:02 +0000

    Added nav bar logo, change input focus styling

--- js/script.js
+++ js/script.js
@@ -8,8 +8,9 @@ function handleError(responseText) {
 
 function drawNavBar() {
 	document.writeln("<div class='nav-bar'>");
+	document.writeln("<img id='nav-bar-logo' src='../img/nav-logo.png'/>");
 	document.writeln("<ul id='left-items'>");
-	document.writeln("<li> <a href='#'>HOME</a>");
+	document.writeln("<li> <a href='../index.php'>HOME</a>");
 	document.writeln("<li> <a href='#'>PROFILE</a>");
 	document.writeln("</ul>");
 	
@@ -18,5 +19,4 @@ function drawNavBar() {
 	document.writeln("</ul>");
 	document.writeln("</div>");
 }
-
-drawNavBar();
+// drawNavBar();
