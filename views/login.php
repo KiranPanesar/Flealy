ba1efbcc727c14606731721b95df9023e1a4f32e
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-20 00:41:59 +0000

    Added login view/js

---
+++ views/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE>
+
+<html>
+
+<head>
+	<title>Flealy - Log In</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="../css/login.css"/>
+	<script src="../js/script.js"></script>
+</head>
+
+<body>
+
+	<div id="login-message">
+		<h1>Log into <a href="#">Flea.ly</a></h1>
+	</div>
+	<br/>
+	<div id="login-div">
+		<form id="login-form">
+			<input type="text" name="username" class="text-input" id="username-input" placeholder="Username" /><br/>
+			<input type="password" name="password" class="text-input" id="password-input" placeholder="Password" /><br/>
+
+			<input type="submit" value="Sign In" class="btn-submit" id="login-up" />
+		</form>
+		<p id="forgotten-login"><a href="http://static1.fjcdn.com/comments/Too+bad+it+isn+t+spiderman+_9f3c27c39099a8f557d70df5b98795c4.jpg" target="_new">Forgotten your password?</a></p>
+
+	</div>
+	<script src="../js/login.js"></script>
+</body>
+
+</html>
