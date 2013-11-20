274746441810378d413745c00b14082117b7fb5b
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-20 00:41:18 +0000

    Rearranged fields

--- views/register.php
+++ views/register.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-	<title>Flealy - Your local online market</title>
+	<title>Flealy - Sign Up</title>
 	<link rel="stylesheet" type="text/css" href="../css/style.css"/>
 	<link rel="stylesheet" type="text/css" href="../css/register.css"/>
 	<script src="../js/script.js"></script>
@@ -17,14 +17,16 @@
 	<br/>
 
 	<div id="signup-div">
-		<input type="text" name="username" class="text-input" id="username-input" placeholder="Username" /><br/><br/>
-		<input type="password" name="password" class="text-input" id="password-input" placeholder="Password" /><br/><br/>
-		<input type="email" name="email" class="text-input" id="email-input" placeholder="Email" /><br/><br/>
+		<form id="signup-form">
+			<input type="email" name="email" class="text-input" id="email-input" placeholder="Email" /><br/>
+			<input type="text" name="username" class="text-input" id="username-input" placeholder="Username" /><br/>
+			<input type="password" name="password" class="text-input" id="password-input" placeholder="Password" /><br/>
 
-		<input type="submit" value="Sign Up" class="btn-submit" id="sign-up" onclick="startShopping();" /><br/>
+			<input type="submit" value="Sign Up" class="btn-submit" id="sign-up" />
+		</form>
 		<input type="submit" value="Home"    class="btn-cancel" id="go-home" onclick="goHome();" />
-
 	</div>
+	<script src="../js/register.js"></script>
 </body>
 
 </html>
