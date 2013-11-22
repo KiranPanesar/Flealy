f532f403d14b3a916d53294cac8a08a3128e1924
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-22 17:19:29 +0000

    /browse now pulls in data from backend, formats and presents

--- js/browse.js
+++ js/browse.js
@@ -1,23 +1,84 @@
-	window.onload = function() {
+window.onload = function() {
+	navigator.geolocation.getCurrentPosition(function(position) {
+		findNearbyItems(position.coords.latitude, position.coords.longitude);
+		initializeMap(position.coords.latitude, position.coords.longitude);
+	});
 
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-			initialize(position.coords.latitude, position.coords.longitude);
-			var positions = new Array();
-			positions[0] = {"lat":51.484764,"lon":-3.173504};
-			positions[1] = {"lat":51.485432,"lon":-3.177087};
-			positions[2] = {"lat":51.485018,"lon":-3.169255};
-			positions[3] = {"lat":51.488959,"lon":-3.172925};
-			positions[4] = {"lat":51.480902,"lon":-3.182173};
-			positions[5] = {"lat":51.489828,"lon":-3.187494};
-			positions[6] = {"lat":51.492820,"lon":-3.170114};
+	// if (navigator.geolocation) {
+	// 	navigator.geolocation.getCurrentPosition(function(position) {
+	// 		initialize(position.coords.latitude, position.coords.longitude);
+	// 		var positions = new Array();
 
-			dropPins(position, positions)
-		});
-	};
+	// 		dropPins(position, positions)
+	// 	});
+	// };
 };
 
-function initialize(lat, lon) {
+var items_json = "";
+
+function findNearbyItems(lat, lon) {
+	var api_request = new XMLHttpRequest();
+	api_request.open("GET", "../api/api.php?action=items&lat="+lat+"&lon="+lon, true);
+	api_request.send();
+
+	api_request.onreadystatechange = function() {
+		if (api_request.readyState == 4) {
+			if (api_request.status != 200) {
+				handleError(api_request.responseText);
+				return;
+			} else {
+				// alert(api_request.responseText);
+				parseItemsJSON(api_request.responseText);
+				// window.location.replace("../views/browse.php")
+			};
+		};
+	}
+}
+
+function parseItemsJSON(items) {
+	var items_table = document.getElementById("items-table");
+
+
+	items_json = JSON.parse(items);
+	
+	var htmlString = "";
+
+	if (items_json.length > 0) {
+		htmlString = "<tr>";
+	
+		for (var i = 0; i < items_json.length; i++) {
+			var item = JSON.parse(items_json[i]);
+
+			if (i % 2 == 0) {
+				htmlString += "</tr>";
+				htmlString += "<tr>";
+			};
+
+			htmlString += "<td>";
+			htmlString += itemHTML(item['name'], item['image_url'], item['price'], item['item_id']);
+			htmlString += "</td>"
+		};
+
+		htmlString += "</tr>";
+	navigator.geolocation.getCurrentPosition(function(position) {
+		dropPins(position);
+	});
+
+	} else {
+		htmlString = "<h2 id='no-items-title'>You're not selling any items :(</h2><br/><center><a class='btn-submit' id='list-button' href='#'>List something!</a></center>"
+	};
+
+	document.getElementById('items-table').innerHTML = htmlString; 
+	document.getElementById("browser").style.opacity = 1.0;
+
+};
+
+function itemHTML(name, image_url, price, id) {
+	var onclickArgument = "showItem("+id+")"
+	return "<div class='item-table-summary' id='" + id + "' onclick='"+onclickArgument+"';> <a href='#'> <img class='item-image' src='"+image_url+"'/><p class='item-name'>" + name + "</p> <p class='item-price'>&pound;" + price + "</p> </a> </div>";
+};
+
+function initializeMap(lat, lon) {
 	var mapOptions = {
 	  center: new google.maps.LatLng(lat, lon),
 	  zoom: 15
@@ -27,19 +88,20 @@ function initialize(lat, lon) {
 }
 
 // pins is an array of Google Maps LatLng() objects.
-function dropPins(position, pins) {
+function dropPins(position) {
 	var mapOptions = {
 	 	zoom: 15,
 	    center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
 	}
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	for (var i = 0; i < pins.length; i++) {
-		var marker = new google.maps.Marker({
-		    position: new google.maps.LatLng(pins[i]['lat'], pins[i]['lon']),
+	for (var i = 0; i < items_json.length; i++) {
+		var item = JSON.parse(items_json[i]);
+
+		var marker = new google.maps.Marker({			
+		    position: new google.maps.LatLng(item.latitude, item.longitude),
 		    map: map,
 		    title: 'Hello World!'
 		});
 	};
-
 }
