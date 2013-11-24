65ae2ca4b7007539c1106dcfc51102ea7872ec0b
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-24 14:45:14 +0000

    Changed button style

--- index.php
+++ index.php
@@ -18,16 +18,16 @@
 
 	<div id="welcome-message">
 		<h1>Welcome to <a href="#">Flea.ly</a></h1>
-		<h2>Your local market</h2>
+		<h2>Find the best items near you.</h2>
 	</div>
 	<br/>
 
 	<div id="get-started">
 		<form id="postcode-form">
 			<input type="text" name="postcode" id="postcode-input" class="text-input" placeholder="Enter your postcode" onkeyup="validatePostcode()" /><br/><br/>
-			<input type="submit" value="Start Shopping" class="btn-submit" id="start-shopping" />
+			<input type="submit" value="Start Shopping" class="btn btn-submit" id="start-shopping" />
 		</form>
-		<input type="submit" value="Sign Up" class="btn-submit" id="sign-up" onclick="signUp();" />
+		<input type="submit" value="Sign Up" class="btn btn-submit" id="sign-up" onclick="signUp();" />
 	</div>
 </body>
 
