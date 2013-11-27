9acc7c453069f1b9848f232b4f8e1dce72f741e7
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-27 10:50:50 +0000

    Added endpoint to load user purchase history

--- api/items.php
+++ api/items.php
@@ -67,4 +67,22 @@ function purchase_item($item_id, $card_id, $transaction_id) {
 	}
 }
 
+function get_purchase_history() {
+	session_start();
+	if (isset($_SESSION['user'])) {
+		$user_id = $_SESSION['user'];
+		$select_query = "SELECT items.item_id, items.name, items.image_url, items.price, purchases.purchase_epoch FROM purchases INNER JOIN items on items.item_id = purchases.item_id AND purchases.buyer_id = '$user_id'";
+		
+		if ($result = db_connection()->query($select_query)) {
+			$results_array = array();
+			
+			while ($row = $result->fetch_assoc()) {
+				$results_array[] = json_encode($row);
+			}
+
+			return json_encode($results_array);
+		}
+	}
+}
+
 ?>
