9cce084b5888cc8c09fa739b6f0760b19b90067d
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-11-29 17:40:39 +0000

    Replaced image for forgotten password

--- views/login.php
+++ views/login.php
@@ -22,7 +22,7 @@
 
 			<input type="submit" value="Sign In" class="btn btn-submit" id="login-up" />
 		</form>
-		<p id="forgotten-login"><a href="http://static1.fjcdn.com/comments/Too+bad+it+isn+t+spiderman+_9f3c27c39099a8f557d70df5b98795c4.jpg" target="_new">Forgotten your password?</a></p>
+		<p id="forgotten-login"><a href="http://f.cl.ly/items/0N332a1C1c3f1X263m3i/Image%202013.11.29%2016%3A00%3A16.jpeg" target="_new">Forgotten your password?</a></p>
 
 	</div>
 	<script src="../js/login.js"></script>
