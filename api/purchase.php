109aa30dc712fd02b26d6c03df6299e244d3b1b8
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-12-02 21:11:04 +0000

    Moved purchase endpoints to purchase.php

---
+++ api/purchase.php
@@ -0,0 +1,36 @@
+<?php
+
+function purchase_item($item_id, $card_id, $transaction_id) {
+	start_session();
+	$purchase_time = time();
+
+	if (isset($_SESSION['user'])) {
+		$user_id = $_SESSION['user'];
+
+		$insert_query = "INSERT INTO purchases (item_id, buyer_id, card_id, stripe_transaction_id, purchase_epoch) VALUES ('$item_id', '$user_id', '$card_id', '$transaction_id', '$purchase_time')";
+
+		if ($result = db_connection()->query($insert_query)) {
+			return json_encode(array('code' => 200, 'message' => 'success'));
+		}
+	}
+}
+
+function get_purchase_history() {
+	start_session();
+	if (isset($_SESSION['user'])) {
+		$user_id = $_SESSION['user'];
+		$select_query = "SELECT items.item_id, items.name, items.image_url, items.price, purchases.purchase_epoch FROM purchases INNER JOIN items on items.item_id = purchases.item_id AND purchases.buyer_id = '$user_id' ORDER BY purchases.purchase_epoch DESC";
+		
+		if ($result = db_connection()->query($select_query)) {
+			$results_array = array();
+			
+			while ($row = $result->fetch_assoc()) {
+				$results_array[] = json_encode($row);
+			}
+
+			return json_encode($results_array);
+		}
+	}
+}
+
+?>
