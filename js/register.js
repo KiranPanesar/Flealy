73484ffe176b1e55761877f0190b7982281f6363
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-12-02 18:02:40 +0000

    Updated registration frontend

--- js/register.js
+++ js/register.js
@@ -1,24 +1,81 @@
-document.getElementById("signup-form").addEventListener('submit', function(event) {
-	event.preventDefault();
+var user_profile_image_base64 = null;
 
-	var email = document.getElementById('email-input').value;
-	var username = document.getElementById('username-input').value;
-	var password = document.getElementById('password-input').value;
+window.onload = function() {
+	document.getElementById("signup-form").addEventListener('submit', function(event) {
+		event.preventDefault();
 
-	var api_request = new XMLHttpRequest();
-	api_request.open("POST", "../api/api.php", true);
-	api_request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	api_request.send('action=user&username=' + username + '&email=' + email + '&password=' + password);
+		var email 	 	= document.getElementById('email-input').value;
+		var username 	= document.getElementById('username-input').value;
+		var password 	= document.getElementById('password-input').value;
+		var city 	  	= document.getElementById('location-input').value;
+		var description = document.getElementById('register-description-text-area').value;
 
-	api_request.onreadystatechange = function() {
-		if (api_request.readyState == 4) {
-			if (api_request.status != 200) {
-				handleError(api_request.responseText);
-				return;
-			} else {				
-				window.location.replace("../views/login.php");
-			};
+		if (validate_registration_form(email, username, password, city, description, user_profile_image_base64)) {
+			var api_request = new XMLHttpRequest();
+			api_request.open("POST", "../api/api.php", true);
+			api_request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+			api_request.send('action=user&username=' + username + '&email=' + email + '&password=' + password + '&location=' + city + '&description=' + description + '&image_data=' + user_profile_image_base64);
+
+			api_request.onreadystatechange = function() {
+				if (api_request.readyState == 4) {
+					if (api_request.status != 200) {
+						handleError(api_request.responseText);
+						return;
+					} else {				
+						window.location.replace("../views/login.php");
+					};
+				};
+			}
 		};
-	}
 
-},  false);
+	},  false);
+
+	document.getElementById('register-file-upload-button').onchange = function() {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+
+			user_profile_image_base64 = e.target.result;
+
+			document.getElementById("register-upload-file-paragraph").parentNode.removeChild(document.getElementById("register-upload-file-paragraph"));
+			document.getElementById("register-upload-file-div").style.backgroundImage = "url("+e.target.result+")";
+
+		}
+
+		reader.readAsDataURL(document.getElementById('register-file-upload-button').files[0]);
+	};
+};
+
+function validate_registration_form(user_email, username, user_password, user_location, user_description, user_image_file) {
+	if (user_email.length == 0) {
+		alert("Enter an email address");
+		return false;
+	};
+
+	if (username.length == 0) {
+		alert("Enter a username");
+		return false;
+	};
+	
+	if (user_password.length == 0) {
+		alert("Enter a password");
+		return false;
+	};
+
+	if (user_location.length == 0) {
+		alert("Enter a location");
+		return false;
+	};
+
+	if (user_description.length == 0) {
+		alert("Enter a description");
+		return false;
+	};
+
+	if (user_image_file == null) {
+		alert("Upload profile picture");
+		return false;
+	};
+
+	return true;
+
+};
--- views/register.php
+++ views/register.php
@@ -18,9 +18,15 @@
 
 	<div id="signup-div">
 		<form id="signup-form">
+			<div id="register-upload-file-div">
+				<p id="register-upload-file-paragraph">Click to add an image</p>
+				<input type="file" id="register-file-upload-button"/><br/>
+			</div>
 			<input type="email" name="email" class="text-input" id="email-input" placeholder="Email" /><br/>
 			<input type="text" name="username" class="text-input" id="username-input" placeholder="Username" /><br/>
 			<input type="password" name="password" class="text-input" id="password-input" placeholder="Password" /><br/>
+			<input type="text" name="location" class="text-input" id="location-input" placeholder="City (e.g. Cardiff)" /><br/>
+			<textarea name="register-description-text-area" id="register-description-text-area" class="text-area" form="signup-form" placeholder="Description"></textarea><br/>
 
 			<input type="submit" value="Sign Up" class="btn btn-submit" id="sign-up" />
 		</form>
