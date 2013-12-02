66aee5b0c99c92271248e2c89ab32b736c7e6b61
Kiran Panesar <KiranSinghPanesar@googlemail.com>
2013-12-02 21:14:01 +0000

    Changed up button colours

--- js/script.js
+++ js/script.js
@@ -19,8 +19,9 @@ function drawNavBar() {
 	document.writeln("<ul id='right-items'>");
 	if (getUserData()) {
 		document.writeln("<li> <a href='../views/user.php'>" + getUserData().username + "</a></li>");
-		document.writeln("<li> <a href='#' class='btn btn-success' onclick='showUserCart()'>CART</a></li>");
-		document.writeln("<li> <a href='#' class='btn btn-info' id='show-list-item-nav-button' onclick='showListItemDialog()'>LIST ITEM</a></li>");
+		document.writeln("<li> <a href='#' class='btn btn-info' onclick='showUserCart()'>CART</a></li>");
+		document.writeln("<li> <a href='#' class='btn btn-info' id='show-list-item-nav-button' onclick='showUserPurchaseHistory()'>PURCHASES</a></li>");
+		document.writeln("<li> <a href='#' class='btn btn-success' id='show-list-item-nav-button' onclick='showListItemDialog()'>LIST ITEM</a></li>");
 	} else {
 		document.writeln("<li> <a href='../views/login.php'>SIGN IN</a></li>");
 	};
